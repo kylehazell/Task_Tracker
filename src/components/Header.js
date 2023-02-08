fc939155336657fba61cb07d26f830b433c5280d
Kyle Hazell <khazell415@gmail.com>
2023-02-08 11:52:49 -0800

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+import { useLocation } from 'react-router-dom'
+
+const Header = ({title, onAdd,  showAdd}) => {
+  const location= useLocation()
+    
+  return (
+    <header className = 'header'>
+        <h1>  {title} </h1>
+        
+       { location.pathname ==='/' && (
+       <Button 
+          color={showAdd ? 'red': 'green'} 
+          text={showAdd ? 'Close' : 'add'} 
+          onClick={onAdd}
+        />
+      )}
+    
+    </header>
+  )
+}
+
+Header.defaultProps={
+    title: 'Task Tracker',
+}
+Header.propTypes={
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle={
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
